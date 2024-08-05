94e0176a5956c3401b7c79bb6975c84c00e526d1
Petya0610 <160774838+Petya0610@users.noreply.github.com>
2024-08-05 20:56:43 +0200

    fix2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,5 @@
-const API_KEY = '45266199-2aebbd1e784974184d81693bb';
-const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = "45266199-2aebbd1e784974184d81693bb";
+const BASE_URL = "https://pixabay.com/api/";
 
 export async function fetchImages(query, page = 1, perPage = 12) {
   const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
